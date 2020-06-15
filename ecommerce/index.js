5508129bee9c829126416c9196f2f82961d616fb
Rodolfo Ugalde 8a <rodolfougaldeochoa@gmail.com>
2020-06-15 16:05:15 -0500

    Update index.js

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -6,7 +6,8 @@ const productsRouter = require('./routes/products');
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-//  SE IMPLEMENTA EL ENPOINT/ RUTA EN /products QUE LA USE EXPRESS app.use 
+//  SE IMPLEMENTA EL ENPOINT/ RUTA EN /products QUE LA USE EXPRESS app.use
+//  EN LA CARPETA routes y dentro el archivo products.js
 app.use("/products", productsRouter);
 
 const server = app.listen(8000, function() {
