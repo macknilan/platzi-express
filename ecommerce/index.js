fdb76c6fb8c6929eefcb616b59e1e5b65b90290f
Rodolfo Ugalde 8a <rodolfougaldeochoa@gmail.com>
2020-06-15 19:52:22 -0500

    Update index.js

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -4,6 +4,8 @@ const app = express();
 const productsRouter = require('./routes/products');
 
 // SE LE DICE A EXPRESS QUE COMO VA A MANEJAR LOS ARCHIVOS ESTATICOS CON EL PREFIJO /static Y LA DIRECCIÓN DE DONDE ESTA LA CARPETA
+// SE CREA LA CARPETA public DENTRO CON LAS CARPETAS assets, images, logo
+// SE MODIFICAN LOS ARCHIVOS .pug DENTRO DE LAS CARPETA view PARA QUE IMPORTEN LOS ARCHIVOS STATIC
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
