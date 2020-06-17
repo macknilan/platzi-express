81c01183f956cdf59cdf18508074f75164d04a26
Rodolfo Ugalde 8a <rodolfougaldeochoa@gmail.com>
2020-06-16 23:43:28 -0500

    Update mongo.js

--- ecommerce/lib/mongo.js
+++ ecommerce/lib/mongo.js
@@ -1,15 +1,22 @@
+// SE IMPORTA LA LIBRERIA DEL DRIVER PARA LA CONEXION
 const { MongoClient } = require("mongodb");
+// ARCHIVO DE CONFIGURACION
 const { config } = require("../config");
 
+// SE IMPORTAN LAS VARIABLES DE CONEXION DEL ARCHIVO DE config/index.js
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const DB_NAME = config.dbName;
 
-const MONGO_URI = `mongodb://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/?authSource=${DB_NAME}`; // prettier-ignore
+// const MONGO_URI = `mongodb://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/?authSource=${DB_NAME}`; // prettier-ignore
+
+// CONFIGURACIÓN PARA CONEXION CON MONGODB ATLAS CON LA LIGA Conect your application
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`; // prettier-ignore
 
 class MongoLib {
   constructor() {
-    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+    console.log(`'MONGO_URI -> ${MONGO_URI}`)
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     this.dbName = DB_NAME;
   }
 
@@ -30,8 +37,8 @@ class MongoLib {
     return this.connect().then(db => {
       return db
         .collection(collection)
-        .find(query)
-        .toArray();
+        .find(query) // QUERY PERSONALIZADO QUE SE OCUPARA PARA LOS TAGS
+        .toArray(); // SE CONVIERTE A ARRELGO PARA QUE SE PUEDA MANIPULAR
     });
   }
 }
